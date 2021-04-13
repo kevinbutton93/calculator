7fd8a73a93a66d2e0d5441547e4a380a76257db3
Kevin Button <kevin.button93@gmail.com>
2021-04-13 11:11:40 +0100

    working, don't touch!

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      input: "",
       output: 0,
       firstNum: "",
       secondNum: "",
@@ -16,79 +15,118 @@ class App extends React.Component {
     this.calculate = this.calculate.bind(this)
     this.clear = this.clear.bind(this)
   }
-
+// Calculate handles the actual math, and sets the state to show the previous calculation above the answer
   calculate(){
     if(this.state.type === "/"){
       this.setState(prevState => {
         let result = Number(prevState.firstNum) / Number(prevState.secondNum)
-        console.log(`${prevState.firstNum} / ${prevState.secondNum} = ${result}`)
-        return {input: result, output: prevState.input, firstNum: result, secondNum: "", type: ""}
+        if(isNaN(result)) return {secondNum: ""};
+        return {output: `${prevState.firstNum} ${prevState.type} ${prevState.secondNum}`, firstNum: result, secondNum: "", type: ""}
       })
     }
     if(this.state.type === "X"){
       this.setState(prevState => {
         let result = Number(prevState.firstNum) * Number(prevState.secondNum)
-        console.log(`${prevState.firstNum} X ${prevState.secondNum} = ${result}`)
-        return {input: result, output: prevState.input, firstNum: result, secondNum: "", type: ""}
+        if(isNaN(result)) return {secondNum: ""};
+        return {output: `${prevState.firstNum} ${prevState.type} ${prevState.secondNum}`, firstNum: result, secondNum: "", type: ""}
       })
     }
     if(this.state.type === "-"){
       this.setState(prevState => {
         let result = Number(prevState.firstNum) - Number(prevState.secondNum)
-        console.log(`${prevState.firstNum} - ${prevState.secondNum} = ${result}`)
-        return {input: result, output: prevState.input, firstNum: result, secondNum: "", type: ""}
+        if(isNaN(result)) return {secondNum: ""};
+        return {output: `${prevState.firstNum} ${prevState.type} ${prevState.secondNum}`, firstNum: result, secondNum: "", type: ""}
       })
     }
     if(this.state.type === "+"){
       this.setState(prevState => {
         let result = Number(prevState.firstNum) + Number(prevState.secondNum)
-        console.log(`${prevState.firstNum} + ${prevState.secondNum} = ${result}`)
-        return {input: result, output: prevState.input, firstNum: result, secondNum: "", type: ""}
+        if(isNaN(result)) return {secondNum: ""};
+        return {output: `${prevState.firstNum} ${prevState.type} ${prevState.secondNum}`, firstNum: result, secondNum: "", type: ""}
       })
     }
+    if(this.state.type === "") this.setState(prevState => ({output: `${prevState.firstNum} ${prevState.type} ${prevState.secondNum}`}))
   }
+// clear function resets all the state to default settings
+  clear(){ this.setState({output: 0, firstNum: "", secondNum: "", type: ""})}
 
-  clear(){ this.setState({input: "", output: 0, firstNum: "", secondNum: "", type: ""})}
-
+// handleClick is called when any button is pressed
+// it reads the name of the button, which matches the displayed text
 
   handleClick(event){
     const name = event.target.name
+    // if "C" is clicked, it runs the "clear" method
     if(name === "C") {
       this.clear()
     }
+    // if "=" is clicked, it runs the calculate method
     if(name === "=") this.calculate()
+    // if "." is clicked, it first checks if a operation has been selected,
+    // it is hasn't it adds a decimal to "state.firstNum"
+    // if it has, it adds a decimal to "state.secondNum" 
+    if(name === "."){
+      if(this.state.type === "") {this.setState(prevState => {
+        if(this.state.firstNum.toString().includes(".")) return
+        return {firstNum: prevState.firstNum + "."}
+      })};
+      if(this.state.type !== "") {this.setState(prevState => {
+        if(this.state.secondNum.toString().includes(".")) return
+        return {secondNum: prevState.secondNum + "."}
+      })}
+    }
 
-    if(name < 10 || name === ".") {
-      if(this.state.input === 0) this.setState({input: ""})
-      if(this.state.type === "") this.setState(prevState => ({firstNum: prevState.firstNum + name,input: prevState.input + name}));
-      if(this.state.type !== "") this.setState(prevState => ({secondNum: prevState.secondNum + name, input: prevState.input + name}))
+    if(name > 0) {
+      if(this.state.type === "") this.setState(prevState => ({firstNum: prevState.firstNum + name}));
+      if(this.state.type !== "") this.setState(prevState => ({secondNum: prevState.secondNum + name}))
+    }
+    if(name === "0") {
+      if(this.state.type === "") {
+        this.setState(prevState => {
+          if(this.state.firstNum.toString()[0] === "0") return 
+          return {firstNum: prevState.firstNum + name}
+        })
+      }
+      if(this.state.type !== "") {
+        this.setState(prevState => {
+          if(this.state.secondNum.toString()[0] ==="0") return
+          return {secondNum: prevState.secondNum + name}
+        })
+      }
     }
     if(name === "/") {
       if(this.state.type !== "") this.calculate()
-      this.setState(prevState => ({input: prevState.input + "/", type: "/"}))
+      this.setState({type: "/"})
     }
     if(name === "X"){
       if(this.state.type !== "") this.calculate()
-      this.setState(prevState => ({input: prevState.input + "X", type: "X"}))
+      this.setState({type: "X"})
     }
     if(name === "-"){
-      if(this.state.type !== "") this.calculate()
-      this.setState(prevState => ({input: prevState.input + "-", type: "-"}))
+      if(this.state.type === "" && this.state.firstNum === "") this.setState({firstNum: "-"})
+      if(this.state.type !== "" && this.state.secondNum === "") this.setState({secondNum: "-"})
+      if(this.state.firstNum !== "" && this.state.type === "") this.setState({type: "-"})
+      if(this.state.secondNum !== "") {
+        this.calculate()
+        this.setState({type: "-"})
+      }
+      
+
     }
     if(name === "+"){
-      if(this.state.type !== "") this.calculate()
-      this.setState(prevState => ({input: prevState.input + "+", type: "+"}))
+      if(this.state.secondNum !== "" || "-") this.calculate()
+      this.setState({type: "+"})
     }
-    console.log(this.state)
+    
   }
 
   render() { 
+    console.log(this.state)
     return (
     <div id="outerContainer" >
       <div id="calculatorContainer">
         <div id="screenContainer">
           <div id="previousCalculation" >{this.state.output}</div>
-          <div id="display" >{this.state.input === "" ? 0 : this.state.input}</div>
+          <div id="display" >{this.state.firstNum === "" ? 0 : `${this.state.firstNum} ${this.state.type} ${this.state.secondNum}`}</div>
         </div>
           <button onClick={this.handleClick} className="button" name="1" id="one">1</button>
           <button onClick={this.handleClick} className="button" name="2" id="two">2</button>
@@ -113,3 +151,4 @@ class App extends React.Component {
 }
 
 export default App;
+
